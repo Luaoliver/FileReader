bb242ac5c6bac61a8551cc9958d35668f8eb6047
Luana <lulu-so@hotmail.com>
2023-05-31 09:47:40 -0300

    feat: handling errors

--- fs.js
+++ fs.js
@@ -3,18 +3,32 @@ const fs = require('fs');
 const readline = require('readline');
 
 //fluxo de leitura
-const fileStream = fs.createReadStream('teste2.docx');
+const filePath = 'teste2.docx';
 
-//tratamento de eventos
-const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-});
+try {
+    if (!fs.existsSync(filePath)) {
+        throw new Error('File not found');
+    }
 
-rl.on('line', (line) => {
-    console.log(`Line: ${line}`);
-});
+    const fileStream = fs.createReadStream(filePath);
 
-rl.on('close', () => {
-    console.log('Finished reading the file.');
-});
+    //tratamento de eventos
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => {
+        console.log(`Line: ${line}`);
+    });
+
+    rl.on('close', () => {
+        console.log('Finished reading the file.');
+    });
+
+    rl.on('error', (err) => {
+        console.error(`Error reading the file: ${err.message}`);
+    });
+} catch (err) {
+    console.error(`Error: ${err.message}`);
+}
